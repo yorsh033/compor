6b4e2c64d8cc4eaf1af79861e017bd64b6742124
yorsh033 <yorsh033@gmail.com>
2015-05-13 12:46:21 -0500

    home template
    creacion del template home

--- home.php
+++ home.php
@@ -4,7 +4,69 @@
 */ 
 ?>
 <?php get_header();?>
-<div class="prueba">
-	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/control-de-acceso4.jpg" alt="">asdas
-</div>
-<?php// wowslider(1); ?>
+
+
+<!-- !!! ACA IRIA EL SLIDER -->
+<section class="Slideshow">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/control-de-acceso4.jpg" alt=""/>
+	<?php// wowslider(1); ?>
+</section>
+
+<!-- SERVICES -->
+<section class="Services">
+	<div class="u-container">
+		<h2 class="u-title">WHAT WE DO</h2>
+		<div class="u-flexbox">
+			<article class="u-col3">
+				<h3 class="u-subtitle">Web Development</h3>
+				<p class="u-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum labore aut, necessitatibus possimus eligendi. Molestiae dolore consectetur eveniet sit, doloremque, blanditiis cumque recusandae pariatur cum est tenetur, nemo, nostrum unde!</p>
+				<a class="u-button"href="#">Read More</a>
+			</article>
+			<article class="u-col3">
+				<h3 class="u-subtitle">Web Design</h3>
+				<p class="u-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum labore aut, necessitatibus possimus eligendi. Molestiae dolore consectetur eveniet sit, doloremque, blanditiis cumque recusandae pariatur cum est tenetur, nemo, nostrum unde!</p>
+				<a class="u-button"href="#">Read More</a>
+			</article>
+			<article class="u-col3">
+				<h3 class="u-subtitle">Computer Sistems</h3>
+				<p class="u-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum labore aut, necessitatibus possimus eligendi. Molestiae dolore consectetur eveniet sit, doloremque, blanditiis cumque recusandae pariatur cum est tenetur, nemo, nostrum unde!</p>
+				<a class="u-button"href="#">Read More</a>
+			</article>
+			<article class="u-col3">
+				<h3 class="u-subtitle">Web Development</h3>
+				<p class="u-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum labore aut, necessitatibus possimus eligendi. Molestiae dolore consectetur eveniet sit, doloremque, blanditiis cumque recusandae pariatur cum est tenetur, nemo, nostrum unde!</p>
+				<a class="u-button"href="#">Read More</a>
+			</article>
+			<article class="u-col3">
+				<h3 class="u-subtitle">Web Design</h3>
+				<p class="u-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum labore aut, necessitatibus possimus eligendi. Molestiae dolore consectetur eveniet sit, doloremque, blanditiis cumque recusandae pariatur cum est tenetur, nemo, nostrum unde!</p>
+				<a class="u-button"href="#">Read More</a>
+			</article>
+			<article class="u-col3">
+				<h3 class="u-subtitle">Computer Sistems</h3>
+				<p class="u-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum labore aut, necessitatibus possimus eligendi. Molestiae dolore consectetur eveniet sit, doloremque, blanditiis cumque recusandae pariatur cum est tenetur, nemo, nostrum unde!</p>
+				<a class="u-button"href="#">Read More</a>
+			</article>
+		</div>
+	</div>
+</section>
+<!-- Products -->
+<section class="Products">
+	<div class="u-container">
+		<h2 class="u-title  u-titleW">OUR PRODUCTS</h2>
+		<div class="u-flexbox">
+			<article class="u-col3">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/products/t2.jpg" alt="T2"/>
+			</article>
+			<article class="u-col3">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/products/3000tc5.jpg" alt="3000-TC"/>
+			</article>
+			<article class="u-col3">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/products/qclearc.jpg" alt="Q-CLEAR-C"/>
+			</article>
+		</div>
+	</div>
+</section>
+
+
+
